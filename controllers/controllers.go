5447b5e8181a237b435317671d027eb0f9d7b6b1
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-01-25 13:05:05 -0300

    feat: new route to show all personalities

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,10 +1,16 @@
 package controllers
 
 import (
+	"apiGO/models"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home page")
+}
+
+func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(models.Personalidades)
 }
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"apiGO/models"
 	"apiGO/routes"
 	"fmt"
 )
 
 func main() {
+
+	models.Personalidades = []models.Personalidade{{Nome: "Nome 1", Historia: "Historia 1"}, {Nome: "Nome 2", Historia: "Historia 2"}}
+
 	fmt.Println("ola go lang :)")
 	routes.HandleRequest()
 }
